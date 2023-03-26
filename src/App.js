443b9d7823483b77fcfb11e2fdbcb22ebb8a5f01
Atar1990 <atarneofeld@gmail.com>
2023-03-26 22:35:39 +0300

    add signup btn

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 
 function App() {
-  const [page, setPage] = useState('signup');
+  const [page, setPage] = useState('login');
 
   return (
     <div className="App">
       <div className="App-content">
-        {page === 'login' && <Login />}
+        {page === 'login' && <Login signupClicked={() => { setPage('signup') }} />}
         {page === 'signup' && <Signup />}
       </div>
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -8,9 +8,9 @@ import FormControl from '@mui/material/FormControl';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { validateEmail } from '../utils/helpers'
-import { Button } from '@mui/material';
+import { Button, Link } from '@mui/material';
 
-function Login() {
+function Login(props) {
     const [showPassword, setShowPassword] = useState(false);
     const [loginFields, setLoginFields] = useState({
         password: '',
@@ -23,42 +23,44 @@ function Login() {
     };
 
     return (
-        <div className="App-login">
-            <img className="App-logo" src="logo.png" />
-            <FormControl sx={{ m: 1, width: 'calc(100% - 16px)' }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
-                <OutlinedInput
-                    error={!!loginFields.email && !validateEmail(loginFields.email)}
-                    onBlur={ev => { setLoginFields({ ...loginFields, email: ev.target.value }) }}
-                    id="outlined-adornment-email"
-                    type="email"
-                    label="Email"
-                />
-            </FormControl>
-            <FormControl sx={{ m: 1, width: 'calc(100% - 16px)' }} variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-password"
-                    type={showPassword ? 'text' : 'password'}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                            >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    onBlur={ev => { setLoginFields({ ...loginFields, password: ev.target.value }) }}
-                    label="Password"
-                />
-            </FormControl>
-            <Button onClick={() => { console.log(loginFields); }} variant="contained">Login</Button>
-            <a></a>
-        </div>
+        <>
+            <div className="App-login">
+                <img className="App-logo" src="logo.png" />
+                <FormControl sx={{ m: 1, width: 'calc(100% - 16px)' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
+                    <OutlinedInput
+                        error={!!loginFields.email && !validateEmail(loginFields.email)}
+                        onBlur={ev => { setLoginFields({ ...loginFields, email: ev.target.value }) }}
+                        id="outlined-adornment-email"
+                        type="email"
+                        label="Email"
+                    />
+                </FormControl>
+                <FormControl sx={{ m: 1, width: 'calc(100% - 16px)' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        onBlur={ev => { setLoginFields({ ...loginFields, password: ev.target.value }) }}
+                        label="Password"
+                    />
+                </FormControl>
+                <Button onClick={() => { console.log(loginFields); }} variant="contained">Login</Button>
+            </div>
+            <Link onClick={() => { props.signupClicked() }}>Signup</Link>
+        </>
     );
 }
 
