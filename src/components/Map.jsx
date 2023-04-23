c5cf093479a771291d7df3ca62001042305b74a6
Atar1990 <atarneofeld@gmail.com>
2023-04-23 20:37:20 +0300

    update final after changing map distance 2

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -180,7 +180,7 @@ function Map(props) {
             <GoogleMap
                 mapContainerStyle={containerStyle}
                 center={center}
-                zoom={8}
+                zoom={5}
                 onLoad={onLoad}
                 onUnmount={onUnmount}
             >
